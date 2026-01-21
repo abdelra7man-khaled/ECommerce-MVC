7f4043e282abe7d47eca2e8ff9b20c19e7206454
Abdelrahman <abdokhm13@gmail.com>
2026-01-22 01:18:06 +0200

    Implmenet Register Action and view

--- Ecommerce.Models/ViewModels/RegisterVM.cs
+++ Ecommerce.Models/ViewModels/RegisterVM.cs
@@ -4,17 +4,20 @@ namespace Ecommerce.Models.ViewModels
 {
     public class RegisterVM
     {
-        [Required(ErrorMessage = "first name field is required"), MaxLength(50)]
+        [Required(ErrorMessage = "First name field is required"), MaxLength(50)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "last name field is required"), MaxLength(50)]
+        [Required(ErrorMessage = "Last name field is required"), MaxLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [Required(ErrorMessage = "email field is required"), EmailAddress, MaxLength(50)]
+        [Required(ErrorMessage = "Email is required"), EmailAddress, MaxLength(50)]
         public string Email { get; set; }
-        [MaxLength(11)]
+        [Required(ErrorMessage = "Phone number is required"), MaxLength(11)]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
-        [Required(ErrorMessage = "address field is required"), MaxLength(200)]
+        [Required(ErrorMessage = "Address field is required"), MaxLength(200)]
         public string Address { get; set; }
-        [Required(ErrorMessage = "city field is required"), MaxLength(100)]
+        [Required(ErrorMessage = "City is required"), MaxLength(100)]
         public string City { get; set; }
         [Required(ErrorMessage = "password field is required"), MaxLength(15)]
         public string Password { get; set; }
---
+++ Ecommerce.Web/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Ecommerce.Models;
+using Ecommerce.Models.ViewModels;
+using Ecommerce.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                UserName = registerVM.Email,
+                Email = registerVM.Email,
+                PhoneNumber = registerVM.PhoneNumber,
+                FirstName = registerVM.FirstName,
+                LastName = registerVM.LastName,
+                Address = registerVM.Address,
+                City = registerVM.City,
+                CreatedAt = DateTime.Now
+            };
+
+            var result = await _userManager.CreateAsync(newUser, registerVM.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, SD.Role_Customer);
+                await _signInManager.SignInAsync(newUser, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            // display general errors
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(registerVM);
+        }
+    }
+}
