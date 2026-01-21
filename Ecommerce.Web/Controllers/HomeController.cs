29779728415ff0c706b9988c0b10340f057c91ff
Abdelrahman <abdokhm13@gmail.com>
2026-01-21 04:03:19 +0200

    displaying Recend products in Home Page

--- Ecommerce.Web/Controllers/HomeController.cs
+++ Ecommerce.Web/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
+using Ecommerce.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(IUnitOfWork _unitOfWork) : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            var products = _unitOfWork.Products.Query()
+                    .Include(p => p.Category)
+                    .Include(p => p.Brand)
+                    .OrderByDescending(p => p.CreatedAt)
+                    .Take(4)
+                    .ToList();
+
+            return View(products);
         }
 
         public IActionResult Privacy()
