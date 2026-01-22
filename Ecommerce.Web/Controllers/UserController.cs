237ce363f8df5502a82bbb02e55d84f69a0edbdc
Abdelrahman <abdokhm13@gmail.com>
2026-01-22 23:35:48 +0200

    Add Users page to admin , display all users

---
+++ Ecommerce.Web/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Web.Controllers
+{
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+        public UserController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IUnitOfWork _uow)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _unitOfWork = _uow;
+        }
+
+        public IActionResult Index()
+        {
+            //var query = _userManager.Users as IQueryable<ApplicationUser>;
+            //var appUsers = query?.OrderByDescending(u => u.CreatedAt).ToList();
+            var appUsers = _unitOfWork.ApplicationUsers.Query()
+                            .OrderByDescending(u => u.CreatedAt)
+                            .ToList();
+
+            return View(appUsers);
+        }
+    }
+}
