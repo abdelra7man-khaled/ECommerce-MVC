b4162fe1119f3522197203869ed03ae130435adc
Abdelrahman <abdokhm13@gmail.com>
2026-01-23 23:35:36 +0200

    display cart product count from server

---
+++ ECommerce.Utility/CartHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace Ecommerce.Utility
+{
+    public class CartHelper
+    {
+        public static Dictionary<int, int> GetCartDictionary(HttpRequest request, HttpResponse response)
+        {
+            string cookieValue = request.Cookies["shopping_cart"] ?? string.Empty;
+
+            try
+            {
+                var cart = Encoding.UTF8.GetString(Convert.FromBase64String(cookieValue));
+                var dictionary = JsonSerializer.Deserialize<Dictionary<int, int>>(cart);
+                if (dictionary != null)
+                {
+                    return dictionary;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            if (cookieValue.Length > 0)
+            {
+                response.Cookies.Delete("shopping_cart");
+            }
+
+            return new Dictionary<int, int>();
+        }
+
+        public static int GetCartSize(HttpRequest request, HttpResponse response)
+        {
+            var cartDictionary = GetCartDictionary(request, response);
+            int size = 0;
+            foreach (var item in cartDictionary)
+            {
+                size += item.Value;
+            }
+            return size;
+        }
+    }
+}
