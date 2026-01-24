2514f74b428fd567e7d66cdb30a204e182a6fb32
Abdelrahman <abdokhm13@gmail.com>
2026-01-25 00:36:14 +0200

    Add Customer's Orders table , so the customer can see his orders history

---
+++ Ecommerce.Web/Controllers/CustomerOrderController.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Web.Controllers
+{
+    public class CustomerOrderController(IUnitOfWork _unitOfWork, UserManager<IdentityUser> _userManager) : Controller
+    {
+        private const int PAGE_SIZE = 5;
+        public async Task<IActionResult> Index(int pageNumber = 1)
+        {
+            var appUser = await _userManager.GetUserAsync(User);
+            if (appUser == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            var query = _unitOfWork.Orders.Query()
+                        .Include(o => o.ApplicationUser)
+                        .Include(o => o.OrderItems)
+                        .ThenInclude(oi => oi.Product)
+                        .OrderByDescending(o => o.OrderDate)
+                        .Where(o => o.ApplicationUserId == appUser.Id)
+                        .AsQueryable();
+
+            int count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / (double)PAGE_SIZE);
+            var orders = query
+                        .Skip((pageNumber - 1) * PAGE_SIZE)
+                        .Take(PAGE_SIZE)
+                        .ToList();
+
+
+            ViewBag.Orders = orders;
+            ViewBag.PageNumber = pageNumber;
+            ViewBag.TotalPages = totalPages;
+
+            return View();
+        }
+    }
+}
