75daf33d7c3f78647670bfe15a7ad4638724afd1
Abdelrahman <abdokhm13@gmail.com>
2026-01-24 23:42:09 +0200

    Add Orders table to admin

---
+++ Ecommerce.Web/Controllers/AdminOrderController.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Web.Controllers
+{
+    [Authorize(Roles = SD.Role_Admin)]
+    public class AdminOrderController(IUnitOfWork _unitOfWork) : Controller
+    {
+        public IActionResult Index()
+        {
+            var orders = _unitOfWork.Orders.Query()
+                        .Include(o => o.ApplicationUser)
+                        .Include(o => o.OrderItems)
+                        .ThenInclude(oi => oi.Product)
+                        .OrderByDescending(o => o.OrderDate)
+                        .ToList();
+
+            ViewBag.Orders = orders;
+
+            return View();
+        }
+    }
+}
