7d253d7cee21fe44f1791b9e5b1ef1568a73f3c3
Abdelrahman <abdokhm13@gmail.com>
2026-01-24 23:47:06 +0200

    Add Pagination to Orders table

--- Ecommerce.Web/Controllers/AdminOrderController.cs
+++ Ecommerce.Web/Controllers/AdminOrderController.cs
@@ -9,16 +9,27 @@ namespace Ecommerce.Web.Controllers
     [Authorize(Roles = SD.Role_Admin)]
     public class AdminOrderController(IUnitOfWork _unitOfWork) : Controller
     {
-        public IActionResult Index()
+        private const int PAGE_SIZE = 5;
+        public IActionResult Index(int pageNumber = 1)
         {
-            var orders = _unitOfWork.Orders.Query()
+            var query = _unitOfWork.Orders.Query()
                         .Include(o => o.ApplicationUser)
                         .Include(o => o.OrderItems)
                         .ThenInclude(oi => oi.Product)
                         .OrderByDescending(o => o.OrderDate)
+                        .AsQueryable();
+
+            int count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / (double)PAGE_SIZE);
+            var orders = query
+                        .Skip((pageNumber - 1) * PAGE_SIZE)
+                        .Take(PAGE_SIZE)
                         .ToList();
 
+
             ViewBag.Orders = orders;
+            ViewBag.PageNumber = pageNumber;
+            ViewBag.TotalPages = totalPages;
 
             return View();
         }
