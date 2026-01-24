87b4c9f9df4bbcaf57a2ab89cf89157346f366b1
Abdelrahman <abdokhm13@gmail.com>
2026-01-24 22:41:24 +0200

    Create Order Summary form

---
+++ Ecommerce.Models/ViewModels/CheckoutVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models.ViewModels
+{
+    public class CheckoutVM
+    {
+        [Required(ErrorMessage = "The Delivery Address is required")]
+        [MaxLength(200)]
+        public string DeliveryAddress { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
--- Ecommerce.Web/Controllers/CartController.cs
+++ Ecommerce.Web/Controllers/CartController.cs
@@ -1,5 +1,7 @@
 ﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Models.ViewModels;
 using Ecommerce.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -33,6 +35,33 @@ namespace Ecommerce.Web.Controllers
 
 
             return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Index(CheckoutVM checkoutVM)
+        {
+            var cartItems = await CartHelper.GetCartItems(Request, Response, _unitOfWork);
+            var cartTotal = CartHelper.GetCartTotal(cartItems);
+            ViewBag.CartItems = cartItems;
+            ViewBag.ShippingCost = _shippingCost;
+            ViewBag.CartTotal = cartTotal;
+            ViewBag.GrandTotal = cartTotal + _shippingCost;
+            if (!ModelState.IsValid)
+            {
+                return View(checkoutVM);
+            }
+
+            if (cartItems.Count == 0)
+            {
+                ViewBag.ErrorMessage = "Your cart is empty";
+                return View(checkoutVM);
+            }
+
+            TempData["DeleveryAddress"] = checkoutVM.DeliveryAddress;
+            TempData["PaymentMethod"] = checkoutVM.PaymentMethod;
+
+            return RedirectToAction("Confirm");
         }
     }
 }
